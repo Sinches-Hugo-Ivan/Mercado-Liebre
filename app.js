863268ad18fa11aadd92e1a89abf75a34013e4bb
Sinches-Hugo-Ivan <hugo.ivan.sinches@gmail.com>
2021-06-05 13:41:18 -0300

    elimine rutas

--- app.js
+++ app.js
@@ -9,12 +9,6 @@ app.use(express.static("public"));
 
 
 app.get("/",(req,res) => res.sendFile(path.resolve(views,"home.html")));
-app.get("/index",(req,res) => res.sendFile(path.resolve(views,"index.html")));
-app.get("/Heroes",(req,res) => res.sendFile(path.resolve(views,"Heroes.html")));
-app.get("/Demonios",(req,res) => res.sendFile(path.resolve(views,"Demonios.html")));
-app.get("/Otros",(req,res) => res.sendFile(path.resolve(views,"Otros.html")));
-app.get("/Pelicula",(req,res) => res.sendFile(path.resolve(views,"Pelicula.html")));
- 
 
 
 app.listen(3030,() => console.log("server on port 3030"));
